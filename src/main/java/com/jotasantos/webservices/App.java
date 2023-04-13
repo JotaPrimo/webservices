3639004c53265da7addd086aa49f4f27aebb8685
sistemasimcg <sistemas@institutomirim.org.br>
2023-04-13 14:05:50 -0300

    JAX-WS - Aula 2: Consumindo o primeiro serviço baseado em SOAP.

--- src/main/java/com/jotasantos/webservices/App.java
+++ src/main/java/com/jotasantos/webservices/App.java
@@ -1,8 +1,12 @@
 package com.jotasantos.webservices;
 
-import com.jotasantos.webservices.servico.CertidaoNascimentoImpl;
 
-import jakarta.xml.ws.Endpoint;
+import java.net.MalformedURLException;
+import java.net.URL;
+import javax.xml.namespace.QName;
+
+import com.jotasantos.webservices.servico.CertidaoNascimento;
+import jakarta.xml.ws.Service;
 
 /**
  * Hello world!
@@ -10,11 +14,25 @@ import jakarta.xml.ws.Endpoint;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws MalformedURLException
     {
-    	CertidaoNascimentoImpl certidaoNascimento = new CertidaoNascimentoImpl();
-        Endpoint.publish("http://localhost:8085/servico/certidao", certidaoNascimento);
-        System.out.println("Serviço publicado com sucesso");
+//    	CertidaoNascimentoImpl certidaoNascimento = new CertidaoNascimentoImpl();
+//        Endpoint.publish("http://localhost:8085/servico/certidao", certidaoNascimento);
+//        System.out.println("Serviço publicado com sucesso");
+    	
+    	URL url = new URL("http://localhost:8085/servico/certidao?wsdl");
+  	  
+        QName qname = new QName("http://servico.webservices.jotasantos.com/", "CertidaoNascimentoImplService");
+  
+        Service service = Service.create(url, qname);
+  
+        CertidaoNascimento certidao = service.getPort(CertidaoNascimento.class);
+        int idade = certidao.calcularIdade("28/01/1995");
+        String diaSemana = certidao.diaSemanaNascimento("28/01/1995");        
+        System.out.println("Idade: " + idade);
+        System.out.println("Dia da semana: " + diaSemana);
 
     }
+    
+    
 }
